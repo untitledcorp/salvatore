2b7e656d8e6827d2b74ec36280096da59c140899
zekkie <zekkiewashere@icloud.com>
2025-03-12 11:21:17 +0100

    idk if the vscode tasks work

--- filesystem.cpp
+++ filesystem.cpp
@@ -1,10 +1,11 @@
-#include "header/filesystem.h"
 #include <iostream>
+#include "header/filesystem.h"
 
-ISO9660Filesystem::ISO9660Filesystem(const std::vector<uint8_t>& partitionData) : data(partitionData) {}
+ISO9660Filesystem::ISO9660Filesystem(const std::vector<uint8_t>& partitionData) 
+    : data(partitionData) {}
 
 void ISO9660Filesystem::detectFilesystem() {
-    if (data[0x8001] == 0x43 && data[0x8002] == 0x44 && data[0x8003] == 0x30 && data[0x8004] == 0x31) {
+    if (data.size() > 0x8004 && data[0x8001] == 0x43 && data[0x8002] == 0x44 && data[0x8003] == 0x30 && data[0x8004] == 0x31) {
         std::cout << "ISO9660 filesystem detected!" << std::endl;
     } else {
         std::cout << "ISO9660 filesystem not detected." << std::endl;
@@ -14,11 +15,12 @@ void ISO9660Filesystem::detectFilesystem() {
 void ISO9660Filesystem::parse() {
     std::cout << "Parsing ISO9660 filesystem..." << std::endl;
 }
-
+FAT32Filesystem::FAT32Filesystem(const std::vector<uint8_t>& partitionData)
+    : data(partitionData) {}
 FAT32Filesystem::FAT32Filesystem(const std::vector<uint8_t>& partitionData) : data(partitionData) {}
 
 void FAT32Filesystem::detectFilesystem() {
-    if (data[0x36] == 0x55 && data[0x37] == 0xAA) {
+    if (data.size() > 0x37 && data[0x36] == 0x55 && data[0x37] == 0xAA) {
         std::cout << "FAT32 filesystem detected!" << std::endl;
     } else {
         std::cout << "FAT32 filesystem not detected." << std::endl;
